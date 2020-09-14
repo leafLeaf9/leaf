a4f73bc11bcb00210481a4301e78463c0eed8e76
woxigousade <woxigsd@gmail.com>
2020-09-14 14:59:24 +0800

    optimize application.properties and rabbitmq configuration
    Signed-off-by: woxigousade <woxigsd@gmail.com>

--- src/main/java/com/gousade/controller/RabbitMQController.java
+++ src/main/java/com/gousade/controller/RabbitMQController.java
@@ -7,9 +7,13 @@ import java.util.Map;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.Binding.DestinationType;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,13 +53,41 @@ public class RabbitMQController {
 		return ResponseResult.renderSuccess().message("发送消息成功。");
 	}
 	
-	@RequestMapping(value="/createExchange",method=RequestMethod.GET)
-	public ResponseResult createExchange() {
-		amqpAdmin.declareExchange(new DirectExchange("amqpAdmin.exchange"));
+	/**
+	 * public Binding(String destination, DestinationType destinationType, String exchange, String routingKey,
+			@Nullable Map<String, Object> arguments)
+			目的地(一般为queue)，目的地类型，exchange,路由键，参数(一般不填)
+	 */
+	@RequestMapping(value="/createExchangeAndQueueAndBind",method=RequestMethod.GET)
+	public ResponseResult createExchangeAndQueueAndBind() {
+		amqpAdmin.declareExchange(new DirectExchange("exchange.direct"));
+		amqpAdmin.declareExchange(new FanoutExchange("exchange.fanout"));
+		amqpAdmin.declareExchange(new TopicExchange("exchange.topic"));
 		log.info("创建exchanges成功");
 		amqpAdmin.declareQueue(new Queue("amqpAdmin.queue"));
-		amqpAdmin.declareBinding(new Binding("amqpAdmin.queue", Binding.DestinationType.QUEUE,"amqpAdmin.exchange","amqpAdmin.queue",null));
-		return ResponseResult.renderSuccess().message("创建exchanges成功");
+		amqpAdmin.declareQueue(new Queue("gousade"));
+		amqpAdmin.declareQueue(new Queue("gousade.news"));
+		amqpAdmin.declareQueue(new Queue("gousade.emps"));
+		amqpAdmin.declareQueue(new Queue("woxigsd.news"));
+		log.info("创建queque成功");
+		amqpAdmin.declareBinding(new Binding("gousade", Binding.DestinationType.QUEUE,"exchange.direct","gousade",null));
+		amqpAdmin.declareBinding(new Binding("gousade.news", Binding.DestinationType.QUEUE,"exchange.direct","gousade.news",null));
+		amqpAdmin.declareBinding(new Binding("gousade.emps", Binding.DestinationType.QUEUE,"exchange.direct","gousade.emps",null));
+		amqpAdmin.declareBinding(new Binding("woxigsd.news", Binding.DestinationType.QUEUE,"exchange.direct","woxigsd.news",null));
+		log.info("exchange.direct绑定queue成功");
+		//fanout不在乎路由键是什么
+		amqpAdmin.declareBinding(new Binding("gousade", Binding.DestinationType.QUEUE,"exchange.fanout","gousade",null));
+		amqpAdmin.declareBinding(new Binding("gousade.news", Binding.DestinationType.QUEUE,"exchange.fanout","",null));
+		amqpAdmin.declareBinding(new Binding("gousade.emps", Binding.DestinationType.QUEUE,"exchange.fanout","",null));
+		amqpAdmin.declareBinding(new Binding("woxigsd.news", Binding.DestinationType.QUEUE,"exchange.fanout","woxigsd.news",null));
+		log.info("exchange.fanout绑定queue成功");
+		amqpAdmin.declareBinding(new Binding("gousade", Binding.DestinationType.QUEUE,"exchange.topic","gousade.#",null));
+		amqpAdmin.declareBinding(new Binding("gousade.news", Binding.DestinationType.QUEUE,"exchange.topic","gousade.#",null));
+		amqpAdmin.declareBinding(new Binding("gousade.news", Binding.DestinationType.QUEUE,"exchange.topic","*.news",null));
+		amqpAdmin.declareBinding(new Binding("gousade.emps", Binding.DestinationType.QUEUE,"exchange.topic","gousade.#",null));
+		amqpAdmin.declareBinding(new Binding("woxigsd.news", Binding.DestinationType.QUEUE,"exchange.topic","*.news",null));
+		log.info("exchange.topic绑定queue成功");
+		return ResponseResult.renderSuccess().message("创建exchanges、queues,并绑定路由键成功");
 	}
 
 }
