9630810cb78839498899c2e127ac46b765a6dc50
woxigousade <53334104+woxigousade@users.noreply.github.com>
2021-08-06 10:07:24 +0800

    docker-maven-plugin

--- src/main/java/com/gousade/utils/Java8DateUtil.java
+++ src/main/java/com/gousade/utils/Java8DateUtil.java
@@ -1,5 +1,6 @@
 package com.gousade.utils;
 
+import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -10,23 +11,34 @@ import java.util.Date;
  * @date 2020/7/21
  */
 public class Java8DateUtil {
-    public static Date ZonedDateTimeToDate(ZonedDateTime zonedDateTime) {
+    public static final ZoneId CTT = ZoneId.of("Asia/Shanghai");
+
+    public static Date zonedDateTimeToDate(ZonedDateTime zonedDateTime) {
         return Date.from(zonedDateTime.toInstant());
     }
 
-    public static ZonedDateTime DateToZonedDateTime(Date date) {
-        return ZonedDateTime.ofInstant(date.toInstant(), ZoneId.of("Asia/Shanghai"));
+    public static ZonedDateTime dateToZonedDateTime(Date date) {
+        return ZonedDateTime.ofInstant(date.toInstant(), CTT);
     }
 
     public static String formatZonedDateTime(ZonedDateTime zonedDateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-                .withZone(ZoneId.of("Asia/Shanghai"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(CTT);
         return zonedDateTime.format(formatter);
     }
 
-    public static ZonedDateTime DateStrToZonedDateTime(String dateStr) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-                .withZone(ZoneId.of("Asia/Shanghai"));
+    public static ZonedDateTime dateTimeStrToZonedDateTime(String dateStr) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(CTT);
         return ZonedDateTime.parse(dateStr, formatter);
+    }
+
+    public static ZonedDateTime dateTimeStrToZonedDateTime(String dateStr, String format) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format).withZone(CTT);
+        return ZonedDateTime.parse(dateStr, formatter);
+    }
+
+    public static ZonedDateTime dateStrToZonedDateTime(String dateStr) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(CTT);
+        LocalDate localDate = LocalDate.parse(dateStr, formatter);
+        return localDate.atStartOfDay(CTT);
     }
 }
