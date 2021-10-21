d862ed1e5b97969d20cead823f809c2cdb4156b5
woxigousade <woxigsd@gmail.com>
2021-10-21 20:13:21 +0800

    ReentrantReadWriteLock test code
    Signed-off-by: woxigousade <woxigsd@gmail.com>

---
+++ src/test/java/com/gousade/art/concurrent/book/chapter05/Cache.java
@@ -0,0 +1,44 @@
+package com.gousade.art.concurrent.book.chapter05;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author woxigousade <woxigsd@gmail.com>
+ * @date 2021/10/21
+ */
+public class Cache {
+	private static final Map<String, Object> map = new HashMap<String, Object>();
+	private static final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+	private static final Lock r = rwl.readLock();
+	private static final Lock w = rwl.writeLock();
+
+	public static final Object get(String key) {
+		r.lock();
+		try {
+			return map.get(key);
+		} finally {
+			r.unlock();
+		}
+	}
+
+	public static final Object put(String key, Object value) {
+		w.lock();
+		try {
+			return map.put(key, value);
+		} finally {
+			w.unlock();
+		}
+	}
+
+	public static final void clear() {
+		w.lock();
+		try {
+			map.clear();
+		} finally {
+			w.unlock();
+		}
+	}
+}
---
+++ src/test/java/com/gousade/art/concurrent/book/chapter05/ProcessData.java
@@ -0,0 +1,43 @@
+package com.gousade.art.concurrent.book.chapter05;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author woxigousade <woxigsd@gmail.com>
+ * @date 2021/10/21
+ */
+public class ProcessData {
+	private static final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+	private static final Lock readLock = rwl.readLock();
+	private static final Lock writeLock = rwl.writeLock();
+	private volatile boolean update = false;
+
+	@Test
+	public void processData() {
+		readLock.lock();
+		if (!update) {
+			// 必须先释放读锁
+			readLock.unlock();
+			// 锁降级从写锁获取到开始
+			writeLock.lock();
+			try {
+				if (!update) {
+					// 准备数据的流程（略）
+					update = true;
+				}
+				readLock.lock();
+			} finally {
+				writeLock.unlock();
+			}
+			// 锁降级完成，写锁降级为读锁
+		}
+		try {
+			// 使用数据的流程（略）
+		} finally {
+			readLock.unlock();
+		}
+	}
+}
