db5d7fd0661692445370327ed7767837706ade41
woxigousade <woxigsd@gmail.com>
2022-12-14 17:37:01 +0800

    feat: add linux skija dependency

--- src/main/java/com/gousade/captcha/carbon/util/MoleculeUtils.java
+++ src/main/java/com/gousade/captcha/carbon/util/MoleculeUtils.java
@@ -3,9 +3,11 @@ package com.gousade.captcha.carbon.util;
 import cn.hutool.core.io.file.LineSeparator;
 import com.gousade.captcha.carbon.MdlMolParser;
 import com.gousade.captcha.carbon.Molecule;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.util.StopWatch;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -21,6 +23,7 @@ import java.util.Random;
  * @author woxigousade <woxigsd@gmail.com>
  * @date 2022/12/03
  */
+@Slf4j
 public class MoleculeUtils {
 	private static final List<Molecule> molecules = init();
 
@@ -29,14 +32,16 @@ public class MoleculeUtils {
 
 	private static List<Molecule> init() {
 		List<Molecule> result = new ArrayList<>();
-		String locationPattern = String.join(File.separator, ResourceUtils.CLASSPATH_URL_PREFIX, "static",
-				"captcha", "carbon", "mol", "*.mol");
+		String locationPattern = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX
+				+ String.join(File.separator, "static", "captcha", "carbon", "mol", "*.mol");
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
 		try {
 			Resource[] resources = new PathMatchingResourcePatternResolver().getResources(locationPattern);
 			Arrays.stream(resources).forEach(resource -> {
 				StringBuilder molStr = new StringBuilder();
 				try (InputStreamReader input = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8);
-					 BufferedReader bufferReader = new BufferedReader(input)) {
+				     BufferedReader bufferReader = new BufferedReader(input)) {
 					String line;
 					while ((line = bufferReader.readLine()) != null) {
 						molStr.append(line).append(LineSeparator.LINUX.getValue());
@@ -50,6 +55,8 @@ public class MoleculeUtils {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		stopWatch.stop();
+		log.info("初始化碳分子库用时{}毫秒,共{}个分子文件。", stopWatch.getTotalTimeMillis(), result.size());
 		return result;
 	}
 
